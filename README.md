ff06136975711ea9e19c54e1bf230170f7ce8ed7
Richie Bendall <richiebendall@gmail.com>
2019-06-29 21:41:35 +1200

    refactor: Update code
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# CloudFlare Badges App
+# Badges Passing
 
 Add badges to your website.
+
+<a href="https://www.cloudflare.com/apps/badges-passing/install?source=button">
+  <img
+    src="https://install.cloudflareapps.com/install-button.png"
+    alt="Install Badges Passing with Cloudflare"
+    border="0"
+    width="150">
+</a>
--- src/index.js
+++ src/index.js
@@ -10,17 +10,45 @@ function init() {
 
     let element
 
+    const removeEmpty = (obj) => {
+        const o = JSON.parse(JSON.stringify(obj)) // Clone source object.
+
+        Object.keys(o).forEach((key) => {
+            if (o[key] && typeof o[key] === "object")
+            {o[key] = removeEmpty(o[key])} // Recurse.
+            else if (o[key] === undefined || o[key] === null)
+            {delete o[key]} // Delete undefined and null.
+            else
+            {o[key] = o[key]} // Copy value.
+        })
+
+        return o // Return new object.
+    }
+
     // updateElement runs every time the options are updated.
     // Most of your code will end up inside this function.
     function updateElement() {
-        options.static.map((val) => {
+        options.badges.map((val) => {
             element = INSTALL.createElement(val.location, element)
             const url = (() => {
-                let o = `https://img.shields.io/badge/${encodeURIComponent(val.label)}-${encodeURIComponent(val.message)}-${encodeURIComponent(val.colour.slice(1))}.svg?style=${options.style}&cacheSeconds=${parseInt(options.cache)}`
-                if (options.style === "social") {
-                    o += `&link=${val.lefturl}&link=${val.righturl}`
+                let p = $.param(removeEmpty({
+                    label: val.label,
+                    color: val.colour.slice(1),
+                    logo: val.logo || "",
+                    style: options.style,
+                    cacheSeconds: parseInt(options.cache),
+                }))
+                if (options.style === "social") p += `&link=${val.lefturl}&link=${val.righturl}`
+                if (val.type === "static") {
+                    const message = encodeURIComponent(val.message)
+                    const label = encodeURIComponent(val.label)
+                    return `https://img.shields.io/badge/${label}-${message}-${encodeURIComponent(val.colour)}.svg?${p}`
                 }
-                return o
+                else if (val.type === "dynamic") {
+                    const url = encodeURIComponent(val.dataurl)
+                    const query = encodeURIComponent(val.dataquery)
+                    return `https://img.shields.io/badge/dynamic/json.svg?${p}&url=${url}&query=${query}`
+                }
             })()
             $(element).replaceWith($("<object>").attr({
                 app: "cloudflare-badges-app",
